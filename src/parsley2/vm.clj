16defa1e77b09a983df0f2d30d5825ee9c1bb7dc
Christophe Grand <christophe@cgrand.net>
2014-08-20 12:36:45 +0200

    less naive

--- src/parsley2/vm.clj
+++ src/parsley2/vm.clj
@@ -1,24 +1,31 @@
 (ns parsley2.core)
 
-(defn merge-stacks [a b]
-  (merge-with merge-stacks a b))
+(defn conj-stacks
+  ([] {})
+  ([stacks] stacks)
+  ([stacks stacks'] (reduce-kv conj-stacks stacks stacks'))
+  ([stacks head tails]
+    (assoc stacks head 
+      (if-some [tails' (stacks head)]
+        (conj-stacks tails' tails)
+        tails))))
 
-(defn push [tails pc] {pc tails})
-
-(defn stepper [pgm]
-  (letfn [(step1 [pc tails c]
-            (case (nth pgm pc)
-              :FORK (merge-stacks (step1 (+ pc 2) tails c) (step1 (nth pgm (inc pc)) tails c)) 
-              :JUMP (recur (nth pgm (inc pc)) tails c)
-              :CALL (recur (nth pgm (inc pc)) (push tails (+ pc 2)) c)
-              :RET (step tails c)
-              :PRED (if ((nth pgm (inc pc)) c)
-                      (push tails (+ pc 2))
-                      {})))
-          (step [stacks c]
-            (reduce-kv (fn [stacks pc tails]
-                         (merge-stacks stacks (step1 pc tails c))) {} stacks))]
-    step))
+(defn stepper
+  ([pgm] (stepper pgm conj-stacks))
+  ([pgm conj-stacks]
+    (letfn [(step1 [pc tails c acc-stacks]
+              (case (nth pgm pc)
+                :FORK (->> acc-stacks (step1 (+ pc 2) tails c) (recur (nth pgm (inc pc)) tails c)) 
+                :JUMP (recur (nth pgm (inc pc)) tails c acc-stacks)
+                :CALL (recur (nth pgm (inc pc)) (push tails (+ pc 2)) c acc-stacks)
+                :RET (step tails c acc-stacks)
+                :PRED (if ((nth pgm (inc pc)) c)
+                        (conj-stacks acc-stacks (+ pc 2) tails)
+                        acc-stacks)))
+            (step [stacks c acc-stacks]
+              (reduce-kv (fn [acc-stacks pc tails]
+                           (step1 pc tails c acc-stacks)) acc-stacks stacks))]
+      #(conj-stacks (step %1 %2 (conj-stacks))))))
 
 (def pgm
   [:FORK 5
